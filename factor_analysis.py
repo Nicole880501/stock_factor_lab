e689dfbd0fe1cd4b95b77fc814754fa4a1184c19
Quen Heng Lee <72889246+QuenHengLee@users.noreply.github.com>
2024-05-10 13:52:01 +0800

    刪除factor_analysis.py模型之間的比較

--- factor_analysis.py
+++ factor_analysis.py
@@ -6,225 +6,161 @@ import pandas as pd
 from iplab.dataframe import CustomDataFrame
 import pandas as pd
 import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+from typing import List, Dict
+from operations import *
 
 
-def cal_interpolated_of_df(df, ascending=False):
-    """
-    INPUTS:
-        df: 想要計算內插值的資料dataframe
-        ascending: 決定因子是越大(F)/小(T)越好, 因子的排序方向
-    RETURN:
-        interpolated_df: 計算完因子內插值後的dataframe
-
-    FUNCTION:
-        以ROW為基準，計算每一ROW的內插值，最大1最小0
-    """
-    # 計算每行的最大值和最小值
-    max_values = df.max(axis=1)
-    min_values = df.min(axis=1)
-
-    # 計算內插值
-    interpolated_df = (df.sub(min_values, axis=0)).div(
-        (max_values - min_values), axis=0
-    )
-
-    # 根據因子的ascending做進一步處理
-    if ascending:
-        return 1 - interpolated_df
-    else:
-        return interpolated_df
-
-
+# 雙因子切割(加權內插/排名法用)
 def cal_factor_sum_df_interpolated(
-    factor_df_dict, factor_ratio_dict, factor_asc_dict, quantile=4
+    factor_name_list: List[str],
+    factor_ratio_dict: Dict[str, float],
+    factor_asc_dict: Dict[str, bool],
+    quantile: int = 4,
+    method: str = 'ranked',
+    all_factor_df_dict: Dict[str, CustomDataFrame] = None,
 ):
     """
-    INPUTS:
-        factor_df_dict: 一個字典，包含多個因子的dataframe，以因子名稱為鍵，對應的dataframe為值
-        factor_ratio_dict: 一個字典，包含多個因子的比重，以因子名稱為鍵，對應的比重為值
-        quantile: 打算將因子切割成幾等分
-    RETURN:
-        factor_sum_df_interpolated: 雙因子內插值相加後的加權總分
-    FUNCTION:
+    Args:
+        factor_name_list (list): 包含多個因子的名稱，例如: factor_name_list = ['roe','pb']
+        factor_ratio_dict (dict): 包含多個因子的比重，以因子名稱為鍵，對應的比重為值
+        factor_asc_dict (dict): 一個字典，包含多個因子的排序方向
+        quantile (positive-int): 打算將因子切割成幾等分
+        all_factor_df_dict (dict): 將所有因子資料的DF存在一個dict當中，例如: all_factor_df_dict = {'roe': roe_data_df, 'pb': pb_data_df, ...}
+    Returns:
+        factor_sum_df_interpolated (dict): 雙因子內插值相加後的加權總分
+    Function:
         該因子選股的方法是根據台股研究室的內插法
         計算多個因子內插值的加權總分，如果有任一因子為nan，其他因子不為nan，則加總也是nan
         最後根據因子切割的大小quantile，回傳該權重的position
     """
-    # 確保輸入的因子數量和比重數量相等
-    if len(factor_df_dict) != len(factor_ratio_dict):
-        raise ValueError("因子數量和比重數量不相等")
+    # 取得個因子名稱
+    factor_1 = factor_name_list[0]
+    factor_2 = factor_name_list[1]
+    factor_df_dict = {}
+    # 判斷雙因子是否相同
+    # 照理來雙因子選股不該帶入兩個相同的因子
+    if factor_1 == factor_2:
+        factor_df_dict[factor_1] = all_factor_df_dict[factor_1]
+        # 將第二個因子KEY值做出名稱差異
+        factor_2 = factor_2 + "'"
+        factor_df_dict[factor_2] = all_factor_df_dict[factor_1]
+        # 比重、排序也要加上第二個重複因子的值\
+        factor_ratio_dict[factor_2] = factor_ratio_dict[factor_1]
+        factor_asc_dict[factor_2] = factor_asc_dict[factor_1]
+    else:
+        factor_df_dict[factor_1] = all_factor_df_dict[factor_1]
+        factor_df_dict[factor_2] = all_factor_df_dict[factor_2]
 
     # 計算因子DF的內插值
-    factor_df_interpolated = {
-        name: cal_interpolated_of_df(df, factor_asc_dict.get(name, False))
-        for name, df in factor_df_dict.items()
-    }
+    # 初始化一個空字典以存儲插值後的數據框
+    factor_df_interpolated = {}
+    # 遍歷 factor_df_dict 中的每一個鍵值對
+    for name, df in factor_df_dict.items():
+        # 從 factor_asc_dict 中取得相應的因子，如果未找到則默認為 False
+        factor = factor_asc_dict.get(name, False)
+        # 調用 cal_interpolated_of_df 函數，傳入當前的數據框和因子
+        # 呼叫計算單因子評分的方法，計算方式可分為: 內插值(interpolated)、排名(ranked)
+        interpolated_df = cal_interpolated_of_df(df, factor,method)
+        # 將結果添加到 factor_df_interpolated 字典中
+        factor_df_interpolated[name] = interpolated_df
 
+ 
     # 將每個因子的內插值乘上對應的比重
     factor_interpolated_weighted = {
         name: interpolated * factor_ratio_dict[name]
         for name, interpolated in factor_df_interpolated.items()
     }
 
-    # 將所有因子的加權內插值相加，得加權總分
-    # 並轉成CustomDataFrame
-    factor_sum_df_interpolated = sum(factor_interpolated_weighted.values())
-    factor_sum_df_interpolated = CustomDataFrame(factor_sum_df_interpolated)
+    # 將所有因子的加權內插值相加，得加權總分，並轉成CustomDataFrame
+    factor_sum_df_interpolated = CustomDataFrame(
+        sum(factor_interpolated_weighted.values())
+    )
 
     # 回傳多因子權重加總後的dataframe
     return factor_sum_df_interpolated.divide_slice(quantile)
 
 
-def factor_analysis_multi_ratio(factor_df_dict, factor_asc_dict, quantile=4):
+# 雙因子切割(過濾篩選多因子)
+def factor_analysis_two_factor_AA(
+    factor_name_list: list,
+    factor_asc_dict: dict,
+    quantile: int = 4,
+    all_factor_df_dict: dict = None,
+) -> dict:
     """
-    INPUTS:
-        factor_df_dict: 一個字典，包含多個因子的dataframe，以因子名稱為鍵，對應的dataframe為值
-        quantile: 打算將因子切割成幾等分
-    RETURN:
-        factor_sum_df_interpolated_dict: 各種因子權重組合的內插值相加後的加權總分，回傳一個包含多個df的dict
-    FUNCTION:
-        該因子選股的方法是根據台股研究室的內插法
-        希望能夠比較不同因子權重的組合表現績效差異
-    """
-    # 回測以下組組合的
-    muiti_ratio_dict = {
-        {
-            "factor1": 0,
-            "factor2": 1,
-        },
-        {
-            "factor1": 0.1,
-            "factor2": 0.9,
-        },
-        {
-            "factor1": 0.2,
-            "factor2": 0.8,
-        },
-        {
-            "factor1": 0.3,
-            "factor2": 0.7,
-        },
-        {
-            "factor1": 0.4,
-            "factor2": 0.6,
-        },
-        {
-            "factor1": 0.5,
-            "factor2": 0.5,
-        },
-        {
-            "factor1": 0.6,
-            "factor2": 0.4,
-        },
-        {
-            "factor1": 0.7,
-            "factor2": 0.3,
-        },
-        {
-            "factor1": 0.8,
-            "factor2": 0.2,
-        },
-        {
-            "factor1": 0.9,
-            "factor2": 0.1,
-        },
-        {
-            "factor1": 1,
-            "factor2": 0,
-        },
-    }
+    實現 Achieving Alpha 的雙因子選股方法(過濾篩選)，
+    強調第一個因子，減弱第二個因子的影響。
 
-    # 每種權重組合都帶入計算
-    for ratio in muiti_ratio_dict:
-        cal_factor_sum_df_interpolated(factor_df_dict, ratio, quantile)
+    Args:
+        factor_name_list (list): 包含多個因子名稱的列表（例如，['roe', 'pb']）。
+        factor_asc_dict (dict): 包含多個因子排序方向的字典。
+        quantile (positive-int): 進行因子切割的分位數。
+        all_factor_df_dict (dict): 包含所有因子資料框的字典
+                                  （例如，{'roe': roe_data_df, 'pb': pb_data_df, ...}）。
 
-    # TODO...
-
-
-# 進行遮罩  5 MASK TRUE = 5,  3 MASK FALSE = NAN
-# Achieving Alpha雙因子會用到
-def MASK(df_bool, df_numeric):
-    # 使用 np.where 進行遮罩操作
-    result = np.where(df_bool, df_numeric, np.nan)
-
-    # 將結果添加到新的 DataFrame 中，並設定相同的日期索引
-    result_df = pd.DataFrame(result, columns=df_numeric.columns, index=df_numeric.index)
-
-    return CustomDataFrame(result_df)
-
-
-def factor_analysis_two_factor_AA(factor_df_dict, factor_asc_dict, quantile=4):
-    """
-    INPUTS:
-        factor_df_dict: 一個字典，包含多個因子的dataframe，以因子名稱為鍵，對應的dataframe為值
-        factor_ratio_dict: 一個字典，包含多個因子的比重，以因子名稱為鍵，對應的比重為值
-        quantile: 打算將因子切割成幾等分
-    RETURN:
-        各分位的position，回傳一個包含多個df的dict
-    FUNCTION:
-        實現Achieving Alpha的雙因子選股方法，強調第一個因子，弱化第二個因子
+    Returns:
+        dict: 包含每個分位數的持倉的字典。
 
     """
-    # 取得因子的list
-    # 取得所有的鍵
-    keys = factor_df_dict.keys()
-    # 將鍵轉換為列表（可選）
-    factor_list = list(keys)
-    # 取得個因子名稱
-    factor_1 = factor_list[0]
-    factor_2 = factor_list[1]
+
+    # 取得個因子名稱()
+    factor_1 = factor_name_list[0]
+    factor_2 = factor_name_list[1]
     # 從Input擷取個因子的DF
-    factor_1_df = factor_df_dict[factor_1]
-    factor_2_df = factor_df_dict[factor_2]
+    factor_1_df = CustomDataFrame(all_factor_df_dict[factor_1])
+    factor_2_df = CustomDataFrame(all_factor_df_dict[factor_2])
     # 從Input擷取個因子的排序方向
     factor_1_asc = factor_asc_dict[factor_1]
     factor_2_asc = factor_asc_dict[factor_2]
     # 先將第一個因子根據quantile值做切割
-    factor_1_slice = CustomDataFrame(factor_1_df).divide_slice(quantile, factor_1_asc)
+    factor_1_slice_dict = factor_1_df.divide_slice(quantile, factor_1_asc)
     # 先進行MASK處理
     factor1_mask_factor2 = {}
-    for q, df in factor_1_slice.items():
+    for q, df in factor_1_slice_dict.items():
         # key = 'Quantile_1_MASK_factor2'
         key = f"{q}_MASK_factor2"
         value = MASK(df, factor_2_df)
         factor1_mask_factor2[key] = value
 
-    # print(factor1_mask_factor2)
-
     result = {}
-    # print("因子:", factor_list)
     for i in range(quantile):
         # key = f"Quantile{i+1}_{factor_1}_{factor_2}"
         key = f"Quantile_{i+1}"
         tmp_str = "Quantile_" + str(i + 1) + "_MASK_factor2"
         tmp_list = factor1_mask_factor2[tmp_str].divide_slice(quantile, factor_2_asc)
         result[key] = tmp_list["Quantile_" + str(i + 1)]
-    print(result)
     return result
 
 
-def factor_analysis_two_factor(factor_df_dict, factor_asc_dict, quantile=4):
+# 雙因子切割(直接做AND交集運算)
+def factor_analysis_two_factor_AND(
+    factor_name_list: list,
+    factor_asc_dict: dict,
+    quantile: int = 4,
+    all_factor_df_dict: dict = None,
+) -> dict:
+    """將兩個因子DF經divide_slice後，根據Quantile 執行AND運算
+
+    Args:
+        factor_name_list (list): 包含多個因子名稱的列表（例如，['roe', 'pb']）。
+        factor_asc_dict (dict): 包含多個因子排序方向的字典。
+        quantile (positive-int): 進行因子切割的分位數。
+        all_factor_df_dict (dict): 包含所有因子資料框的字典
+                                  （例如，{'roe': roe_data_df, 'pb': pb_data_df, ...}）。
+
+    Returns:
+        dict: 包含每個分位數的持倉的字典。
+
     """
-    INPUTS:
-        factor_df_dict: 一個字典，包含多個因子的dataframe，以因子名稱為鍵，對應的dataframe為值
-        factor_ratio_dict: 一個字典，包含多個因子的比重，以因子名稱為鍵，對應的比重為值
-        quantile: 打算將因子切割成幾等分
-    RETURN:
-        各分位的position，回傳一個包含多個df的dict
-    FUNCTION:
-        將兩個因子DF經divide_slice後，根據Quantile 執行AND運算
-    """
-    # 取得因子的list
-    # 取得所有的鍵
-    keys = factor_df_dict.keys()
-    # 將鍵轉換為列表（可選）
-    factor_list = list(keys)
+
     # 取得個因子名稱
-    factor_1 = factor_list[0]
-    factor_2 = factor_list[1]
+    factor_1 = factor_name_list[0]
+    factor_2 = factor_name_list[1]
     # 從Input擷取個因子的DF
-    factor_1_df = factor_df_dict[factor_1]
-    factor_2_df = factor_df_dict[factor_2]
+    factor_1_df = all_factor_df_dict[factor_1]
+    factor_2_df = all_factor_df_dict[factor_2]
     # 從Input擷取個因子的排序方向
     factor_1_asc = factor_asc_dict[factor_1]
     factor_2_asc = factor_asc_dict[factor_2]
@@ -245,79 +181,26 @@ def factor_analysis_two_factor(factor_df_dict, factor_asc_dict, quantile=4):
     return result
 
 
-def factor_analysis_single(factor_df_dict, factor_asc_dict, quantile=4):
+# 單因子切割
+def factor_analysis_single(factor_df, factor_asc: bool, quantile: int = 4) -> dict:
     """
-    INPUTS:
-        factor_df_dict: 一個字典，包含多個因子的dataframe，以因子名稱為鍵，對應的dataframe為值
-        factor_asc_dict: 一個字典，包含多個因子的排序方向，越大/小越好
-        quantile: 打算將因子切割成幾等分
-    RETURN:
-        各分位的position，回傳一個包含多個df的dict
-    FUNCTION:
-        將兩個因子DF經divide_slice後，根據Quantile 執行AND運算
-    """
-    if len(factor_df_dict) == 1 and len(factor_asc_dict) == 1:
-        # 取得所有的鍵
-        keys = factor_df_dict.keys()
-        # 將鍵轉換為列表（可選）
-        factor_name_list = list(keys)
-        # 取得個因子名稱
-        factor_name = factor_name_list[0]
-        factor_value_df = factor_df_dict[factor_name]
-        factor_asc = factor_asc_dict[factor_name]
-        return factor_value_df.divide_slice(quantile, factor_asc)
-    else:
-        print("該方法為單因子切割，請勿帶入超過一個因子")
+    單因子根據值的大小與排序方向做分割
 
+    Args:
+        factor_df (dataframe): 單一因子的資料
+        factor_asc (bool): 排序的方向，F:越大越好; T:越小越好
+        quantile (positive-int): 打算將因子切割成幾等分
+
+    Returns:
+        各分位的position，回傳一個包含多個df的dict
+    """
+
+    return factor_df.divide_slice(quantile, factor_asc)
+
+
+ 
+ 
 
 if __name__ == "__main__":
-    # 生成 10x10 的隨機數據，其中大約 20% 的元素為 NaN
-    np.random.seed(42)
-    data = np.random.randint(0, 1000, size=(100, 100)).astype(float)
-    mask = np.random.choice([True, False], size=data.shape, p=[0.2, 0.8])
-    data[mask] = np.nan
-    # 將數據轉換成 DataFrame，並使用日期範圍作為索引
-    factor_1 = pd.DataFrame(
-        data,
-        columns=[f"Col{i+1}" for i in range(100)],
-        index=pd.date_range(start="2022-01-01", periods=100, freq="D"),
-    )
-
-    # 生成 10x10 的隨機數據，其中大約 20% 的元素為 NaN
-    np.random.seed(98)
-    data = np.random.randint(0, 1000, size=(100, 100)).astype(float)
-    mask = np.random.choice([True, False], size=data.shape, p=[0.2, 0.8])
-    data[mask] = np.nan
-    # 將數據轉換成 DataFrame，並使用日期範圍作為索引
-    factor_2 = pd.DataFrame(
-        data,
-        columns=[f"Col{i+1}" for i in range(100)],
-        index=pd.date_range(start="2022-01-01", periods=100, freq="D"),
-    )
-
-    # 示例使用方式
-    factor_df_dict = {
-        "ROE": CustomDataFrame(factor_1),
-        "PB": CustomDataFrame(factor_2),
-        # 可以加入其他因子
-    }
-
-    factor_ratio_dict = {
-        "ROE": 0.5,
-        "PB": 0.5,
-        # 可以加入其他比重
-    }
-    factor_asc_dict = {
-        "ROE": False,
-        "PB": False,
-    }
-    # 呼叫台股研究室的內積加權法cal_factor_sum_df_interpolated()
-    # result = cal_factor_sum_df_interpolated(
-    #     factor_df_dict, factor_ratio_dict, factor_asc_dict
-    # )
-
-    # 呼叫Achieving Alpha的雙因子選股方法factor_analysis_two_factor_AA()
-    result = factor_analysis_two_factor_AA(factor_df_dict, factor_asc_dict)
-
-    # # 呼叫直接兩因子做AND運算方法
-    # result = factor_analysis_two_factor(factor_df_dict, factor_asc_dict)
+    pass
+ 
